512e37ac37026119b74f4b902f3bd3b2546211ec
jake <booker_jake@yahoo.co.uk>
2019-01-09 15:29:57 +0000

    refactoring code

--- src/main/scala/BottlesOfBeer2.scala
+++ src/main/scala/BottlesOfBeer2.scala
@@ -3,8 +3,8 @@ object BottlesOfBeer2 extends App{
     println(s"$counter bottles of beer on the wall, $counter bottles of beer. ")
     println(s"Take one down and pass it around, $counter bottles of beer on the wall.\n")
   }
-  val a = 99 to 1 by -1
-  a.foreach(i => (bottlefunction(i)))
+  val rangeOfNums = 99 to 1 by -1
+  rangeOfNums.foreach(i => bottlefunction(i))
 
   println(s"No more bottles of beer on the wall, no more bottles of beer. ")
   println(s"Go to the store and buy some more, 99 bottles of beer on the wall.")
--- src/main/scala/DuplicateInts.scala
+++ src/main/scala/DuplicateInts.scala
@@ -5,34 +5,34 @@ object DuplicateInts extends App {
 
   val array1 = Array(768395, 768395, 954687, 463527, 463527, 100000)
   // Find maximum number in array
-  def findMax(x:Int, y:Int):Int = {
-    val top = x max y
-    println("compared %d to %d, %d was larger".format(x,y,top))
+  def findMax(num1:Int, num2:Int):Int = {
+    val top = num1 max num2
+    println("compared %d to %d, %d was larger".format(num1,num2,top))
     top
   }
   array1.reduceLeft(findMax)
 
   // Find duplicate integers in an array with reduceLeft (IN PROGRESS - DOES NOT COMPILE)
-  def duplicate(x:Int, y:Int):Int = {
-    val m = x + y
-    val n = m/2
-    if(n == x){
-      println("compared %d to %d, %d is a duplicate".format(x,y,n))
+  def duplicate(num1:Int, num2:Int):Int = {
+    val sum = num1 + num2
+    val sumDivided = sum/2
+    if(sumDivided == num1){
+      println("compared %d to %d, %d is a duplicate".format(num1,num2,sumDivided))
     }
-    else if(n == y) {
-      println("compared %d to %d, %d is a duplicate".format(x,y,n))
+    else if(sumDivided == num2) {
+      println("compared %d to %d, %d is a duplicate".format(num1,num2,sumDivided))
     }
     else{
-      println("compared %d to %d, not a duplicate".format(x,y))
+      println("compared %d to %d, not a duplicate".format(num1,num2))
     }
-    n
+    sumDivided
   }
   array1.reduceLeft(duplicate)
 
   // Find duplicate integers in an array with foldLeft (IN PROGRESS - DOES NOT COMPILE)
   val a = Array(1,2,3,4)
-  def add(a: Int, b: Int):Int = {
-    a + b
+  def add(num1: Int, num2: Int):Int = {
+    num1 + num2
   }
   println(foldLeft(0)(a)(add))
 
